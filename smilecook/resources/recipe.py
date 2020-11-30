d5576489f17f09a65a20bbc311ec80c03bc1d6e8
Carl Hinderer <carl.hinderer4@gmail.com>
2020-11-30 15:07:26 -0700

    Implemented deleting of recipes.

--- smilecook/resources/recipe.py
+++ smilecook/resources/recipe.py
@@ -57,6 +57,16 @@ class RecipeResource(Resource):
 
         return recipe.data, HTTPStatus.OK
 
+    def delete(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id \
+            and recipe.is_publish == True), None)
+
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        recipe_list.remove(recipe)
+        return {}, HTTPStatus.NO_CONTENT
+
 
 class RecipePublishResource(Resource):
 
