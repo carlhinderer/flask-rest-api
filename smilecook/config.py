7ed0c7ed1c6ff2dea401368e18bf5a60fddd257d
Carl Hinderer <carl.hinderer4@gmail.com>
2020-11-30 22:06:48 -0700

    Installed and configured Flask-JWT-Extended.

--- smilecook/config.py
+++ smilecook/config.py
@@ -3,3 +3,6 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql://smilecookuser:smilecookpw@localhost/smilecook'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = '8VWu6vzzA9jfj6qz'
+    JWT_ERROR_MESSAGE_KEY = 'message'
