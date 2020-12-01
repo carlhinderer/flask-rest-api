3ffbdb992fd993ad950251b17e0331d7fe6b7210
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-01 11:24:57 -0700

    Added /me endpoint for getting details for logged in user.

--- smilecook/app.py
+++ smilecook/app.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 
 from config import Config
 from extensions import db, jwt
-from resources.user import UserListResource, UserResource
+from resources.user import UserListResource, UserResource, MeResource
 from resources.token import TokenResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
@@ -27,6 +27,7 @@ def register_resources(app):
 
     api.add_resource(UserListResource, '/users')
     api.add_resource(UserResource, '/users/<string:username>')
+    api.add_resource(MeResource, '/me')
     api.add_resource(TokenResource, '/token')
     
     api.add_resource(RecipeListResource, '/recipes')
