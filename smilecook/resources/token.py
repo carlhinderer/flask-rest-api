25e298f60a7552fa24b57c8e458b7a1dbc8804cd
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-04 21:33:32 -0700

    Added new resource and route for generating a refresh token.

--- smilecook/resources/token.py
+++ smilecook/resources/token.py
@@ -1,7 +1,12 @@
 from http import HTTPStatus
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import (
+    create_access_token, 
+    create_refresh_token,
+    jwt_refresh_token_required,
+    get_jwt_identity
+)
 
 from utils import check_password
 from models.user import User
@@ -20,6 +25,17 @@ class TokenResource(Resource):
         if not user or not check_password(password, user.password):
             return {'message': 'username or password is incorrect'}, HTTPStatus.UNAUTHORIZED
 
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(identity=user.id)
 
-        return {'access_token': access_token}, HTTPStatus.OK
+
+        return {'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK
+
+
+class RefreshResource(Resource):
+
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user, fresh=False)
+        return {access_token: access_token}, HTTPStatus.OK
