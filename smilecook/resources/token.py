23edf8e48fb090a6f6392c4a44c7f94b1e74ad7e
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-04 23:40:42 -0700

    Added user logout.

--- smilecook/resources/token.py
+++ smilecook/resources/token.py
@@ -1,15 +1,21 @@
 from http import HTTPStatus
 from flask import request
 from flask_restful import Resource
+
 from flask_jwt_extended import (
-    create_access_token, 
+    create_access_token,
     create_refresh_token,
     jwt_refresh_token_required,
-    get_jwt_identity
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt
 )
 
 from utils import check_password
 from models.user import User
+
+
+black_list = set()
 
 
 class TokenResource(Resource):
@@ -38,4 +44,13 @@ class RefreshResource(Resource):
     def post(self):
         current_user = get_jwt_identity()
         access_token = create_access_token(identity=current_user, fresh=False)
-        return {access_token: access_token}, HTTPStatus.OK
+        return {'access_token': access_token}, HTTPStatus.OK
+
+
+class RevokeResource(Resource):
+
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        black_list.add(jti)
+        return {'message': 'Successfully logged out'}, HTTPStatus.OK
