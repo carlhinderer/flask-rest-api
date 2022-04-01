44f9f6220a482b009a466a868b30022475340639
Carl Hinderer <carl.hinderer4@gmail.com>
2022-03-31 20:41:46 -0600

    Added generation and verification of the account activation token.

--- smilecook/config.py
+++ smilecook/config.py
@@ -2,12 +2,14 @@ class Config:
     # Flask
     DEBUG = True
 
+    # Secrets
+    SECRET_KEY = 'super-secret-key'
+
     # SqlAlchemy
     SQLALCHEMY_DATABASE_URI = 'postgresql://smilecookuser:smilecookpw@localhost/smilecook'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # JWT
-    SECRET_KEY = 'super-secret-key'
     JWT_ERROR_MESSAGE_KEY = 'message'
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
