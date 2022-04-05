a215d381780c13462b133d46c2d5a3d4cadb58b2
Carl Hinderer <carl.hinderer4@gmail.com>
2022-04-04 18:01:05 -0600

    Added ability to pass in custom sort order when retrieving recipe list.

--- smilecook/models/recipe.py
+++ smilecook/models/recipe.py
@@ -25,9 +25,14 @@ class Recipe(db.Model):
     def get_all_published(cls, q, page, per_page):
         keyword = '%{keyword}%'.format(keyword=q)
 
+        if order == 'asc':
+            sort_logic = asc(getattr(cls, sort))
+        else:
+            sort_logic = desc(getattr(cls, sort))
+
         return cls.query.filter(or_(cls.name.ilike(keyword), cls.description.ilike(keyword)),
                                 cls.is_publish.is_(True)).\
-                         order_by(desc(cls.created_at)).\
+                         order_by(sort_logic).\
                          paginate(page=page, per_page=per_page)
 
     @classmethod
